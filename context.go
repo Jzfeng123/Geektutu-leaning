f75fcf1eecdf8955a96aa3b5b133766c199a767f
jzf <jiangzhifeng0307@163.com>
2023-07-20 18:40:11 +0800

    修改上下文

--- context.go
+++ context.go
@@ -73,11 +73,22 @@ func (c *Context) Form(key string) (string, error) {
 		c.cacheBody = c.req.Body
 	}
 	// 取数据
-	err := c.req.ParseForm()
+	err := c.req.ParseForm() //ParseForm无法取出JSON格式的数据
+	// "application/x-www-form-urlencoded"只能取出这一种格式的数据, 因此得自己定义一个使之能读
 	if err != nil {
 		return "", err
 	}
 	return c.req.FormValue(key), nil
+}
+
+// 获取JSON格式的请求体数据
+func (c *Context) BindJSON(dest any) error {
+	if c.cacheBody == nil {
+		c.cacheBody = c.req.Body
+	}
+	decoder := json.NewDecoder(c.cacheBody) // Decoder解码
+	decoder.DisallowUnknownFields()         //处理未知字符
+	return decoder.Decode(dest)
 }
 
 // 新建一个上下文，就相当于是一个视图函数
@@ -113,16 +124,16 @@ func (c *Context) SetData(data []byte) {
 // y以上三种方式是小零件，需要配合其他的方法给用户使用 --> JSON, HTML, String
 // 纯文本类型
 func (c *Context) String(code int, data string) {
-	c.SetStatusCode(code)
 	c.SetHeader("Content-Type", "text/plain")
+	c.SetStatusCode(code)
 	c.SetData([]byte(data))
 }
 
 // JSON 响应JSON格式数据
 func (c *Context) JSON(code int, data any) {
-	c.SetStatusCode(code)
 	c.SetHeader("Content-Type", "application/json")
-	res, err := json.Marshal(data)
+	c.SetStatusCode(code)
+	res, err := json.Marshal(data) //序列化处理
 	if err != nil {
 		// 1.存在的问题：如果panic报错
 		// 我们之前设置的响应头和状态码需要去掉吗？最好是去掉
@@ -134,22 +145,25 @@ func (c *Context) JSON(code int, data any) {
 }
 
 // HTML 响应HTML
-func (c *Context) HTML(code int, data string) {
-	c.SetStatusCode(code)
+func (c *Context) HTML(code int, html string) {
 	c.SetHeader("Content-Type", "text/html")
-	c.SetData([]byte(data))
+	//c.SetHeader("", "text/html")
+	c.SetStatusCode(code)
+	c.SetData([]byte(html))
 }
 
-// 既然我们没有直接把响应数据写入Header中，那么我们就需要自己建立一个方法将数据读进去
+// FlashToHeader 既然我们没有直接把响应数据写入Header中，那么我们就需要自己建立一个方法将数据读进去
+// 写入的顺序为响应头-状态码-响应体，顺序错的话则不能正确输出
+// 这是golang的一个坑
 func (c *Context) FlashToHeader() {
+	// 写入响应头
+	for key, val := range c.header {
+		c.w.Header().Set(key, val) //优先级最低所以放最上面先去执行
+	}
 	// 写入状态码
 	c.w.WriteHeader(c.StatusCode)
-	// 写入状态头
-	for key, val := range c.header {
-		c.w.Header().Set(key, val)
-	}
-	// 写入状态体
-	c.w.Write(c.data)
+	// 写入响应体
+	_, _ = c.w.Write(c.data)
 }
 
 //我们在Context上下文中，维护一些请求相关的数据是因为可能有很多视图函数需要用到这些数据
